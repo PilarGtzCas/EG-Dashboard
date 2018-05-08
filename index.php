a3c9323ec411d6fc612e9cf13a6618af6f1fcd92
PilarGtzCas <39073220+PilarGtzCas@users.noreply.github.com>
2018-05-08 08:52:47 -0500

    v1

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Acceso Denegado</title>
+<style type="text/css" media="screen">
+body{
+	background-color: #000;
+	margin: 0px
+}
+#ctabla table #ctexto td {
+	font-family: Calibri;
+	color: #FFF;
+}
+.ctexto1 {
+	font-family: Calibri;
+	color: #FFF;
+	text-align: right;
+}
+.titulo3 {
+	font-family: Calibri;
+	color: #666666;
+		text-align: right;
+}
+
+#ctabla{
+	font-family: Verdana, Geneva, Arial, sans-serif;
+	background-color: #0000;
+	text-align: center;
+	vertical-align: middle;
+	margin-top: -145px;
+	margin-left: -400px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	width: 800px;
+	height: 290px;
+}
+
+a:link, a:visited{
+	color: #666;
+	text-decoration: none
+}
+
+a:hover{
+	color: #fff;
+	text-decoration: none
+}
+
+</style>
+</head>
+
+<body>
+<DIV id="ctabla">
+<table width="200" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr align="center" bottom="middle">
+    <td colspan="3" valign="middle" bgcolor="#1B1B1B" class="ctexto1">Advertencia!</td>
+  </tr>
+  <tr>
+    <td colspan="3" bgcolor="#060606"><img src="img/denegado.jpg" width="800" height="250" /></td>
+  </tr>
+  <tr id="ctexto">
+    <td width="622" bgcolor="#484848">&nbsp;</td>
+    <td width="67" bgcolor="#1B1B1B" class="ctexto1" style="font-family: 'Arial Narrow'; text-align: center;"><span class="titulo3"><a href="acceso.php">Login</a></span></td>
+    <td width="99" bgcolor="#333333" class="ctexto1" style="font-family: 'Arial Narrow'; color: #FFF; text-align: center;"><span class="titulo3"><a href="registro.php">Subscribirme</a></span></td>
+  </tr>
+</table>
+</DIV>
+
+</body>
+</html>
